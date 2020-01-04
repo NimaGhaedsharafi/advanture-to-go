94f0fd8cf07c40a95610eeaa18ebcd2754844ac1
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2020-01-04 11:57:44 +0330

    fixes directory name

--- template/Template.go
+++ template/Template.go
@@ -4,12 +4,12 @@ import (
 	"html/template"
 	"log"
 	"path/filepath"
-	"testing/Util"
+	"testing/util"
 )
 
 func Setup() *template.Template {
 	tmpl := template.New("")
-	tmpl.Funcs(template.FuncMap{"Strong": Util.Strong})
+	tmpl.Funcs(template.FuncMap{"Strong": util.Strong})
 	_, e := tmpl.ParseGlob(filepath.Join(".", "*.html"))
 	if e != nil {
 		log.Fatalln(e)
