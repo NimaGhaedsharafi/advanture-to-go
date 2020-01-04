06a95774f89f115721723623b7e46272f6aed85e
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2020-01-04 12:35:41 +0330

    extracts http handler from main

--- main.go
+++ main.go
@@ -2,29 +2,14 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"net/http"
 	"os"
-	. "testing/contact"
-	"testing/template"
+	"testing/app/http"
 )
-
 var (
-	listenAdrs = flag.String("adrs", os.Getenv("HTTP_ADRS"), "HTTP address to listen on")
-	tmpl       = template.Setup()
+	adrs = flag.String("adrs", os.Getenv("HTTP_ADRS"), "HTTP address to listen on")
 )
-
 func main() {
 	flag.Parse()
 
-	http.HandleFunc("/", index)
-	e := http.ListenAndServe(*listenAdrs, nil)
-	if e != nil {
-		panic("Couldn't start the server Error: " + e.Error())
-	}
-	fmt.Printf("It listens to %s\n", *listenAdrs)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	tmpl.ExecuteTemplate(w, "index.html", struct{ Contacts []*Contact }{GetAll()})
+	http.Serve(adrs)
 }
