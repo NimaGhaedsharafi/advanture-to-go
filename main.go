b27d2ebc4b36487d42ea7d6e30e315c4ffe791e2
Nima Ghaedsharafi <ghaedsharafi.nima@gmail.com>
2020-01-04 12:03:04 +0330

    refactors db from main

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/jmoiron/sqlx"
 	"net/http"
 	"os"
-	"testing/connection"
 	. "testing/contact"
 	"testing/template"
 )
@@ -14,24 +12,12 @@ import (
 var (
 	listenAdrs = flag.String("adrs", os.Getenv("HTTP_ADRS"), "HTTP address to listen on")
 	tmpl       = template.Setup()
-	db         *sqlx.DB
 )
 
 func main() {
 	flag.Parse()
-	db := connection.Connection()
-
-	if db == nil {
-		os.Exit(2)
-	}
-
-	_, e := db.Exec("select * from contacts")
-
-	if e != nil {
-		os.Exit(3)
-	}
-
 	fmt.Printf("It listens to %s\n", *listenAdrs)
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(*listenAdrs, nil)
 }
